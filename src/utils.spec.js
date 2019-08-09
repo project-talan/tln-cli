32e83cff5a5210947f250eb3027a763378db0a14
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2019-08-09 10:43:02 +0000

    unit tests for utils.js

--- src/utils.spec.js
+++ src/utils.spec.js
@@ -1,12 +1,59 @@
+'use strict';
+
+const path = require('path');
+const fs = require('fs');
+
 const chai = require('chai');
 const expect = chai.expect;
+const sinon = require('sinon');
+
 const utils = require('./utils');
 
+//
+beforeEach(function () {
+})
+
+afterEach(function () {
+})
+
+//
 describe('utils', function() {
-  it('Configuration file name', function() {
-    expect(utils.getConfFile('path/to/conf')).to.be.equal('path/to/conf/.tln.conf');
+  const componentHome = path.join('path', 'to', 'component', 'home');
+
+  it('Get configuration file name', function() {
+    expect(utils.getConfFile(componentHome)).to.be.equal(path.join(componentHome, '.tln.conf'));
   });
-  it('Configuration folder name', function() {
-    expect(utils.getConfFolder('path/to/folder/with/conf')).to.be.equal('path/to/folder/with/conf/.tln');
+  it('Get configuration folder name', function() {
+    expect(utils.getConfFolder(componentHome)).to.be.equal(path.join(componentHome, '.tln'));
+  });
+  it('Check if configurations (file or/and folder) present', function() {
+    let existsSyncStub = sinon.stub(fs, 'existsSync').callsFake(function (p) {
+      return false;
+    });
+    expect(utils.isConfPresent(componentHome)).to.be.false;
+    expect(existsSyncStub.called).to.be.true;
+    existsSyncStub.restore();
+    //
+    existsSyncStub = sinon.stub(fs, 'existsSync').callsFake(function (p) {
+      return (p == path.join(componentHome, '.tln.conf'));
+    });
+    expect(utils.isConfPresent(componentHome)).to.be.true;
+    expect(existsSyncStub.called).to.be.true;
+    existsSyncStub.restore();
+    //
+    existsSyncStub = sinon.stub(fs, 'existsSync').callsFake(function (p) {
+      return (p == path.join(componentHome, '.tln'));
+    });
+    expect(utils.isConfPresent(componentHome)).to.be.true;
+    expect(existsSyncStub.called).to.be.true;
+    existsSyncStub.restore();
+    //
+    existsSyncStub = sinon.stub(fs, 'existsSync').callsFake(function (p) {
+      return ((p == path.join(componentHome, '.tln.conf')) || (p == path.join(componentHome, '.tln')));
+    });
+    expect(utils.isConfPresent(componentHome)).to.be.true;
+    expect(existsSyncStub.called).to.be.true;
+    existsSyncStub.restore();
+    //
   });
 });
