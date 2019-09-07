849623b33e00dcefcbc5499e6069f93540e1d086
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2019-09-07 10:45:37 +0000

    cmake/maven/linux

--- presets/utils.js
+++ presets/utils.js
@@ -27,7 +27,11 @@ module.exports = {
         r.push(`powershell -Command "Remove-Item '${name}'"`);
       } else if (platform === 'linux') {
         r.push(`wget '${url}'`);
-        r.push(`tar -xzf '${name}'`);
+        if (name.match('tar.gz')) {
+          r.push(`tar -xzf '${name}'`);
+        } else {
+          r.push(`unzip '${name}'`);
+        }
         // move content
         if (opts) {
           if (opts[0] && opts[1]) {
