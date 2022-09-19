35ddc04b57a168ebc04b1532c982caba73b8ccd9
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2022-09-19 06:36:41 +0000

    feat: load env vars during script execution and not from script itself

--- src/utils.js
+++ src/utils.js
@@ -169,5 +169,49 @@ module.exports = {
       }
     }
     return {name, version};
+  },
+  parseEnvRecord(line, logger, errMsg = 'Command line argument (-e | --env) is incorrect') {
+    if (line && line.length) {
+      const record = line.trim();
+      if (record.length) {
+        const delim = record.indexOf('=');
+        let key = record;
+        let value = '';
+        if (delim >= 0) {
+          key = record.substring(0, delim).trim();
+          value = record.substring(delim+1).trim();
+        }
+        if (key.length) {
+          return {[key]: value};
+        }
+      }
+    }
+    if (logger) {
+      logger.error(`${errMsg}: '${line}'`);
+    }
+  },
+  parseEnvFile(fileName, logger, errMsg = 'Specified file (--env-file) is not found') {
+    if (fileName) {
+      if (fs.existsSync(fileName)) {
+        let env = {};
+        let ln = 0;
+        fs.readFileSync(fileName, 'utf-8')
+          .split('\n')
+          .filter(Boolean)
+          .forEach(line => {
+            ln++;
+            if (line.trim().length) {
+              const e = module.exports.parseEnvRecord(line, logger, `Env var record in line: ${ln} from: '${fileName}' is incorrect`);
+              if (e) {
+                env = {...env, ...e};
+              }
+            }
+          });
+        return env;
+      }
+    }
+    if (logger) {
+      logger.error(`${errMsg}: '${fileName}'`);
+    }
   }
 }
