3210965ba4869979d3a82a862802c54aea03d253
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2023-01-30 09:29:09 +0200

    fix: flutter base url

--- components/grunt/components.js
+++ components/grunt/components.js
@@ -1,1 +1,1 @@
-module.exports = [{"id":"grunt-1.5.3"},{"id":"grunt-1.5.2"},{"id":"grunt-1.5.1"},{"id":"grunt-1.5.0"},{"id":"grunt-1.4.1"},{"id":"grunt-1.4.0"},{"id":"grunt-1.3.0"},{"id":"grunt-1.2.1"},{"id":"grunt-1.2.0"},{"id":"grunt-1.1.0"}];
+module.exports = [{"id":"grunt-1.6.0"},{"id":"grunt-1.5.3"},{"id":"grunt-1.5.2"},{"id":"grunt-1.5.1"},{"id":"grunt-1.5.0"},{"id":"grunt-1.4.1"},{"id":"grunt-1.4.0"},{"id":"grunt-1.3.0"},{"id":"grunt-1.2.1"},{"id":"grunt-1.2.0"},{"id":"grunt-1.1.0"}];
