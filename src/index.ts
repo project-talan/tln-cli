7ff5fb3c496a3dbac8d376aac601f3b177efa944
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2026-08-01 16:22:02 +0300

    feat(001): local dev setup

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 
 function main(argv: readonly string[]): void {
   console.log('Hello from your new TypeScript CLI!');
+  console.log('!!!!!!!!!!!!!');
 
   if (argv.length > 0) {
     console.log(`Arguments received: ${argv.join(', ')}`);
