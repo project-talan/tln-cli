90589868ef1f57f437a6684abef04809d6c025f6
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2026-08-01 22:30:13 +0300

    feat(001): cleanup code

--- src/util/globalOptions.ts
+++ src/util/globalOptions.ts
@@ -8,33 +8,25 @@ import type { Options } from 'yargs';
  */
 export interface GlobalArgv {
   verbose: number;
+  dryRun: boolean;
+  all: boolean;
+  depth: number;
   parallel: boolean;
   recursive: boolean;
-  all: boolean;
-  dryRun: boolean;
   env: string[];
-  depth: number;
-  failOnStderr: boolean;
-  parentFirst: boolean;
-  catalog: string[];
   envFile: string[];
-  localRepo: string | undefined;
-  detach: boolean;
+  failOnStderr: boolean;
   '--': string[];
 }
 
 export const globalOptions = {
   verbose: { alias: 'v', count: true, default: 0, describe: 'Increase logging verbosity' },
+  'dry-run': { alias: 'u', type: 'boolean', default: false, describe: "Don't do anything, just print generated scripts" },
+  all: { alias: 'a', type: 'boolean', default: false, describe: 'Show all components' },
+  depth: { alias: 'd', type: 'number', default: 1, describe: 'Depth level' },
   parallel: { alias: 'p', type: 'boolean', default: false, describe: 'Execute commands for multiple components in parallel' },
   recursive: { alias: 'r', type: 'boolean', default: false, describe: 'Execute commands recursively for all direct child components' },
-  all: { alias: 'a', type: 'boolean', default: false, describe: 'Show all components' },
-  'dry-run': { alias: 'u', type: 'boolean', default: false, describe: "Don't do anything, just print generated scripts" },
   env: { alias: 'e', type: 'array', string: true, default: [], describe: 'Set environment variables' },
-  depth: { alias: 'd', type: 'number', default: 1, describe: 'Max depth level' },
-  'fail-on-stderr': { type: 'boolean', default: true, describe: 'Stop execution when script returns an error' },
-  'parent-first': { type: 'boolean', default: false, describe: 'During recursive execution, parent will be processed first and then nested components' },
-  catalog: { type: 'array', string: true, default: [], describe: 'URL to the external components description' },
   'env-file': { type: 'array', string: true, default: [], describe: 'Read in a file of environment variables' },
-  'local-repo': { type: 'string', describe: "Shared components will be deployed using this path or project's root otherwise, if parameter is not defined" },
-  detach: { type: 'boolean', default: false, describe: 'Shared components will be deployed inside tmp folder' },
+  'fail-on-stderr': { type: 'boolean', default: true, describe: 'Stop execution when script returns an error' },
 } satisfies Record<string, Options>;
